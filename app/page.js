6bed76d92a0f3c452f7dc1d4e62789c2b55d54cc
Diyac004 <diyachaudhari004@gmail.com>
2024-08-16 17:24:49 -0700

    third commit

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-'use client'
+
 import { useState,useEffect } from "react";
 import {firestore} from '@/firebase';
 import { collection, doc, getDocs, query, setDoc, getDoc, deleteDoc} from "firebase/firestore";
